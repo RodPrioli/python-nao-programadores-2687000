071ace91eb5a3908c8eefab5b314f8cf19c7b280
RodPrioli <mprioli@gmail.com>
2025-01-22 14:44:42 +0000

    n complexo

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,14 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+string = 'é variavel de texto tudo que estiver entre as aspas simples ou dupla será texto mesmo se aparecer um valor 1'
 # Declare variáveis de cada tipo numérico
-
-
+n_inteiro = 143
+n_float   = 1,4
+complexo = 2+1j
 # Declare uma variável do tipo booleano
+
+verdadeiro = True
+falso      = False
+
+
